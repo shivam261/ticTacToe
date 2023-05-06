93ae0f8d901b9903a10b54c4f89fc3e42c81fdb6
shivam <shivam1705of@gmail.com>
2023-05-06 10:02:09 +0530

    added dialog

--- app/src/main/java/com/example/tictac/MainActivity.java
+++ app/src/main/java/com/example/tictac/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.tictac;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.GridLayout;
@@ -76,6 +78,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert).setTitle("be alert").setMessage("welcome to the game").setNeutralButton("play", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(),"game started",Toast.LENGTH_SHORT).show();
+            }
+        }).show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
